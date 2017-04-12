771922b21704c3e9bb349ca666d5b20adae08489
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-04-12 11:52:06 +0700

    group config

--- LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
+++ LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
@@ -3,6 +3,10 @@ package com.github.hellocore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import com.github.hellocore.model.User;
 
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -21,6 +25,19 @@ public class LocalRedisConfig {
        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);
        return jedisConnectionFactory;
     }
-    
+
+	
+    @Bean
+    public RedisTemplate<String, User> userTemplate(JedisConnectionFactory jedisConnectionFactory) {
+    	RedisTemplate<String, User> userTemplate = new RedisTemplate<>();
+    	Jackson2JsonRedisSerializer<User> serializer = new Jackson2JsonRedisSerializer<>(User.class);
+    	userTemplate.setConnectionFactory(jedisConnectionFactory);
+    	userTemplate.setDefaultSerializer(serializer);
+    	userTemplate.setHashKeySerializer(serializer);
+    	userTemplate.setHashValueSerializer(serializer);
+        userTemplate.afterPropertiesSet();
+
+        return userTemplate;
+    }
 
 }
