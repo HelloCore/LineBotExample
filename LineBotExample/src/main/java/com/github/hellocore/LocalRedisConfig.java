8cf981437f0700768c76be1ef37cce2be65ab875
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-04-12 11:27:29 +0700

    Add configuration

--- LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
+++ LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
+import com.github.hellocore.model.User;
+
+import jnr.ffi.types.clock_t;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
@@ -22,6 +27,19 @@ public class LocalRedisConfig {
        poolConfig.setTestWhileIdle(true);
        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);
        return jedisConnectionFactory;
+    }
+    
+    @Bean
+    public RedisTemplate<String, User> userTemplate(RedisConnectionFactory redisConnectionFactory) {
+    	RedisTemplate<String, User> userTemplate = new RedisTemplate<>();
+    	Jackson2JsonRedisSerializer<User> serializer = new Jackson2JsonRedisSerializer<>(User.class);
+    	userTemplate.setConnectionFactory(redisConnectionFactory);
+    	userTemplate.setDefaultSerializer(serializer);
+    	userTemplate.setHashKeySerializer(serializer);
+    	userTemplate.setHashValueSerializer(serializer);
+        userTemplate.afterPropertiesSet();
+
+        return userTemplate;
     }
 
 }
