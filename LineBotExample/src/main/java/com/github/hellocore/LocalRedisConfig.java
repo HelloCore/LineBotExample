8cecf8e7d817b210a33930af1d6eef4610a98cd7
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-04-12 11:49:20 +0700

    seperate config

--- LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
+++ LineBotExample/src/main/java/com/github/hellocore/LocalRedisConfig.java
@@ -2,12 +2,7 @@ package com.github.hellocore;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-
-import com.github.hellocore.model.User;
 
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -15,12 +10,11 @@ import redis.clients.jedis.JedisPoolConfig;
 public class LocalRedisConfig {
 
     @Bean
-    public RedisConnectionFactory jedisConnectionFactory(){
+    public JedisConnectionFactory jedisConnectionFactory(){
        JedisPoolConfig poolConfig = new JedisPoolConfig();
        poolConfig.setMaxTotal(10);
        poolConfig.setMinIdle(1);
        poolConfig.setMaxIdle(5);
-       
        poolConfig.setTestOnBorrow(true);
        poolConfig.setTestOnReturn(true);
        poolConfig.setTestWhileIdle(true);
@@ -28,17 +22,5 @@ public class LocalRedisConfig {
        return jedisConnectionFactory;
     }
     
-    @Bean
-    public RedisTemplate<String, User> userTemplate(RedisConnectionFactory redisConnectionFactory) {
-    	RedisTemplate<String, User> userTemplate = new RedisTemplate<>();
-    	Jackson2JsonRedisSerializer<User> serializer = new Jackson2JsonRedisSerializer<>(User.class);
-    	userTemplate.setConnectionFactory(redisConnectionFactory);
-    	userTemplate.setDefaultSerializer(serializer);
-    	userTemplate.setHashKeySerializer(serializer);
-    	userTemplate.setHashValueSerializer(serializer);
-        userTemplate.afterPropertiesSet();
-
-        return userTemplate;
-    }
 
 }
