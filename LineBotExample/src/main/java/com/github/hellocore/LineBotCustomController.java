a678e8041e72a5f559e24666473217cbbfbcc43c
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-04-12 14:58:51 +0700

    remove error

--- LineBotExample/src/main/java/com/github/hellocore/LineBotCustomController.java
+++ LineBotExample/src/main/java/com/github/hellocore/LineBotCustomController.java
@@ -3,9 +3,11 @@ package com.github.hellocore;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.github.hellocore.model.Group;
 import com.github.hellocore.model.User;
@@ -28,6 +30,7 @@ public class LineBotCustomController {
 	private GroupRepository groupRepository;
     
 	@RequestMapping("/echo")
+	@ResponseStatus(value = HttpStatus.OK)
 	public void echo(@RequestParam String message){
 		List<User> listSender = userRepository.listUser();
 		for (User user : listSender) {
@@ -37,6 +40,7 @@ public class LineBotCustomController {
 	
 
 	@RequestMapping("/GroupEcho")
+	@ResponseStatus(value = HttpStatus.OK)
 	public void groupEcho(@RequestParam String message){
 		List<Group> listGroup = groupRepository.listGroup();
 		for (Group group : listGroup) {
