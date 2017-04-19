78e36d86d140cad6064d1120400a8bbf49b23a76
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-04-20 01:58:29 +0700

    Hello

--- LineBotExample/src/main/java/com/github/hellocore/LineBotCustomController.java
+++ LineBotExample/src/main/java/com/github/hellocore/LineBotCustomController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.github.hellocore.model.Group;
@@ -47,4 +48,21 @@ public class LineBotCustomController {
 			lineMessagingClient.pushMessage(new PushMessage(group.getGroupId(), new TextMessage(message)));
 		}
 	}
+	
+	
+	@RequestMapping("/virus11.tx")
+	public @ResponseBody String virus11tx(){
+		return "1";
+	}
+	
+	@RequestMapping("/virus22.tx")
+	public @ResponseBody String viru22tx(){
+		return "Hello World!!!";
+	}
+
+	@RequestMapping("/virus33.tx")
+	public @ResponseBody String viru33tx(){
+		return "A16078870033C4898424";
+	}
+	
 }
